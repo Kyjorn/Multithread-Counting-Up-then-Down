37b3921063516d4108785c4144c395fd648c1718
Kyjorn <hendonmshane@gmail.com>
2025-05-11 13:45:28 -0600

    Add Counter and Main classes for counting functionality

---
+++ ThreadCounter/Counter.java
@@ -0,0 +1,12 @@
+package ThreadCounter;
+
+public class Counter {
+    
+    //counting up
+    public void countUp(){
+        System.out.println("Counting up...");
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ ThreadCounter/Main.java
@@ -0,0 +1,12 @@
+package ThreadCounter;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //initialize counters
+        Counter cUp = new Counter();
+
+        cUp.countUp();
+    }
+}
