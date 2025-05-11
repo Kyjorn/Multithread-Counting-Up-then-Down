8588ceb51af6df749f6b84f43059253df6525a09
Kyjorn <hendonmshane@gmail.com>
2025-05-11 14:04:29 -0600

    Update counting limits in Counter class to 20 for both countUp and countDown methods

--- ThreadCounter/Counter.java
+++ ThreadCounter/Counter.java
@@ -3,20 +3,20 @@ package ThreadCounter;
 
 public class Counter {
     
-    //counts up to 5
+    //counts up to 20
     public void countUp () {
         System.out.println("Counting up...");
-        for(int i = 1; i <= 5; i++){
+        for(int i = 1; i <= 20; i++){
             System.out.println(i);
         }
     }
 
 
 
-    //counts down from 5
+    //counts down from 20
     public void countDown () {
         System.out.println("Counting down...");
-        for(int i = 5; i >= 1; i--){
+        for(int i = 20; i >= 1; i--){
             System.out.println(i);
         }
         System.out.println("Count completed.");
