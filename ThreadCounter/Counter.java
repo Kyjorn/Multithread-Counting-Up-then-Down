98544b99e39693544045e23ab7dd2e37466812e8
Kyjorn <hendonmshane@gmail.com>
2025-05-11 13:50:50 -0600

    Created countDown method and added calls in Main

--- ThreadCounter/Counter.java
+++ ThreadCounter/Counter.java
@@ -1,12 +1,22 @@
 package ThreadCounter;
 
+
 public class Counter {
     
-    //counting up
-    public void countUp(){
+    //counts up to 5
+    public void countUp () {
         System.out.println("Counting up...");
         for(int i = 1; i <= 5; i++){
             System.out.println(i);
         }
+    }
+
+    //counts down from 5
+    public void countDown () {
+        System.out.println("Counting down...");
+        for(int i = 5; i >= 1; i--){
+            System.out.println(i);
+        }
+        System.out.println("Count completed.");
     }
 }
--- ThreadCounter/Main.java
+++ ThreadCounter/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
         
         //initialize counters
-        Counter cUp = new Counter();
+        Counter cUp = new Counter(); //initalizes an up counter
+        Counter cDown = new  Counter(); //initializes a down counter
 
+        //calls to 'Counter'
         cUp.countUp();
+        cDown.countDown();
     }
 }
